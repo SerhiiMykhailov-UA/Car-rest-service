421945c4c5ecd8eac2c0d14fb2aa54f2c73c21bd
Serhii Mykhailov <serhii.mykhailov@yahoo.com>
2024-03-16 20:43:00 +0200

    problem with uuid

--- src/main/java/ua/foxminded/entity/Car.java
+++ src/main/java/ua/foxminded/entity/Car.java
@@ -2,16 +2,18 @@ package ua.foxminded.entity;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -33,8 +35,9 @@ public class Car implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+    @GeneratedValue(generator = "uuid-hibernate-generator")
+    @GenericGenerator(name = "uuid-hibernate-generator", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
 	
 	@NonNull
 	@Column
